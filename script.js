9426f4c8fc060c2a3d812b865df35fdcad8798cd
TaniaVervier <tvervier@gmail.com>
2022-02-24 15:07:49 +0100

    m

---
+++ script.js
@@ -0,0 +1,32 @@
+let msg1 = document.getElementById("message1");
+let msg2 = document.getElementById("message2");
+let msg3 = document.getElementById("message3");
+
+
+let answer = Math.floor(Math.random()*100) +1;
+let n_of_guesses = 0;
+let guessed_numbers = [];
+
+function play(){
+    let user_guess = document.getElementById("guess").value;
+    if(user_guess < 1 || user_guess > 100){
+        alert("Please enter a number between 1 and 100.");
+    } else {
+        guessed_numbers.push(user_guess);
+        n_of_guesses+= 1;
+        if(user_guess < answer){
+            msg1.textContent = "Too low, try again";
+            msg2.textContent = "N° of guesses : " + n_of_guesses;
+            msg3.textContent = "Guessed numbers are: " + guessed_numbers
+        } else if (user_guess > answer){
+            msg1.textContent = "Too high, try again";
+            msg2.textContent = "N° of guesses : " + n_of_guesses;
+            msg3.textContent = "Guessed numbers are: " + guessed_numbers
+        }
+        else if(user_guess == answer){
+            msg1.textContent = "YOU WIN!";
+            msg2.textContent = "The number was: " + answer;
+            msg3.textContent = "You guessed it in " + n_of_guesses + "guesses";
+        }
+    }
+}
